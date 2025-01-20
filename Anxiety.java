d25ab700940e688d45d033bf469961f4053b98b5
Sean Chen <schen200826@gmail.com>
2025-01-20 16:42:15 -0500

    wrote constructors

--- Anxiety.java
+++ Anxiety.java
@@ -5,5 +5,29 @@ public class Anxiety extends Adventurer{
     private boolean debuff = false;
     private boolean buff = false;
     private int maxHP = 21;
+    public int getSpecial(){
+        return neg;
+      }
+      public Anxiety(String name, int hp, int neg, int specialMax){
+        super(name,hp);
+        this.specialMax = specialMax;
+        this.neg = neg;
+      }
+    
+      public Anxiety(String name, int hp){
+        this(name, hp, 0, 10);
+      }
+    
+      public Anxiety(String name){
+        this(name, 21);
+      }
+    
+      public Anxiety(){
+        this("Anxiety");
+      }
+    
+      public String getSpecialName(){
+        return specialName;
+      }
     
 }
