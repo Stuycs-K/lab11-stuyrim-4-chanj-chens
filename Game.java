4bc01da1cca36bb458ec1c843b225a2d9e5a87b9
Jessie Chan <jessiechan55555@gmail.com>
2025-01-20 19:25:23 -0500

    changed charactesr namnes

--- Game.java
+++ Game.java
@@ -235,9 +235,9 @@ public class Game{
     ArrayList<Adventurer> party = new ArrayList<>();
     /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
     //YOUR CODE HERE
-    party.add(createRandomAdventurer("Mr. K"));
-    party.add(createRandomAdventurer("Mr. K"));
-    party.add(createRandomAdventurer("Mr. K"));
+    party.add(createRandomAdventurer("Riley"));
+    party.add(createRandomAdventurer("Riley"));
+    party.add(createRandomAdventurer("Riley"));
     
     /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 
@@ -263,13 +263,13 @@ public class Game{
         enemies.add(new Boss());
       }
       if (input.equals("2")){
-        enemies.add(createRandomAdventurer("Mr. S"));
-        enemies.add(createRandomAdventurer("Mr. S"));
+        enemies.add(createRandomAdventurer("AI"));
+        enemies.add(createRandomAdventurer("AI"));
       }
       if (input.equals("3")){
-        enemies.add(createRandomAdventurer("Mr. S"));
-        enemies.add(createRandomAdventurer("Mr. S"));
-        enemies.add(createRandomAdventurer("Mr. S"));
+        enemies.add(createRandomAdventurer("AI"));
+        enemies.add(createRandomAdventurer("AI"));
+        enemies.add(createRandomAdventurer("AI"));
       }
       
     }
